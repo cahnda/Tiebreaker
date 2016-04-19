46808b87a1d161607eb3934ed061ff8aeab250c2
cahnda <cahnda@gmail.com>
2016-04-18 21:46:17 -0400

    Adjustments for deployment

--- app.py
+++ app.py
@@ -107,11 +107,14 @@ def results():
     	session["currentResults"] = currentResults
     	for x in session["currentResults"]: 
     		print x
-    	queryResults = currentResults.get(str(session["HIT_ID"]))
-    	print "CURRENT RESULTS LENGTH"
-    	print len(currentResults)
-    	print "QUERY RESULTS LENGTH"
-    	print len(queryResults)
+        try: 
+        	queryResults = currentResults.get()
+        	print "CURRENT RESULTS LENGTH"
+        	print len(currentResults)
+        	print "QUERY RESULTS LENGTH"
+        	print len(queryResults)
+        except:
+            print str(session["HIT_ID"]) + "NOT IN SESSION"
     	session['data_array'] = utils.getTurkResults(queryResults,session["option_1"],session["option_2"])
     	arr = session['data_array']
     	if (session['added_DB'] == 0):
---
+++ py/getKeys.py
@@ -0,0 +1,5 @@
+def stripePrivate():
+	return "sk_test_tk6tmmWXCJhAf1dIp0wUisuL"
+
+def getMongo():
+	return "NETS213"
